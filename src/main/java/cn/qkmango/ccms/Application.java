a22d30983bffc360ed11aaecf59836c08669fe1b
qkmango <2099432021@qq.com>
2023-09-06 20:08:04 +0800

    添加注释

--- src/main/java/cn/qkmango/ccms/Application.java
+++ src/main/java/cn/qkmango/ccms/Application.java
@@ -3,13 +3,12 @@ package cn.qkmango.ccms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @EnableCaching
-// @EnableScheduling
+/**
+ * @author qkmango
+ */
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
